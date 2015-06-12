8159d40e9d728922a0e9eca6dbbd0cb089b09ce7
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-06-12 15:50:35 +0200

    Deploy latest component. Requires deploy, environment and tarballs symlinks

---
+++ deploy-latest.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+echo "Looking for latest of: ${1}"
+tarball=`ls ${1}* | sort | tail -1`
+echo "Deploying: ${tarball}"
+./deploy/scripts/deploy.sh ${tarball} -i environment/inventory -l app.stepup.example.com
