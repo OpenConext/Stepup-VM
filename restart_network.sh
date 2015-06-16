3053406bb80fc779080caf8d4d1282210c537289
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-06-16 08:37:47 +0200

    Reformat

--- restart_network.sh
+++ restart_network.sh
@@ -1,11 +1,6 @@
 #!/bin/sh
-echo app: network restart
-vagrant ssh app -c "sudo /etc/init.d/network restart"
-echo manage: network restart
-vagrant ssh manage -c "sudo /etc/init.d/network restart"
-echo lb: network restart
-vagrant ssh lb -c "sudo /etc/init.d/network restart"
-echo db: network restart
-vagrant ssh db -c "sudo /etc/init.d/network restart"
-echo ks: network restart
-vagrant ssh ks -c "sudo /etc/init.d/network restart"
+echo "app: network restart"; vagrant ssh app -c "sudo /etc/init.d/network restart" 
+echo "manage: network restart"; vagrant ssh manage -c "sudo /etc/init.d/network restart" 
+echo "lb: network restart"; vagrant ssh lb -c "sudo /etc/init.d/network restart" 
+echo "db: network restart"; vagrant ssh db -c "sudo /etc/init.d/network restart" 
+echo "ks: network restart"; vagrant ssh ks -c "sudo /etc/init.d/network restart" 
