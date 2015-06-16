78f7a83457fbbc8b7a20acceefb4a36948e499e8
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2015-06-16 08:37:01 +0200

    Add keyserver

--- deploy-latest.sh
+++ deploy-latest.sh
@@ -2,4 +2,4 @@
 echo "Looking for latest of: ${1}"
 tarball=`ls ${1}* | sort | tail -1`
 echo "Deploying: ${tarball}"
-./deploy/scripts/deploy.sh ${tarball} -i environment/inventory -l app.stepup.example.com
+./deploy/scripts/deploy.sh ${tarball} -i environment/inventory -l app.stepup.example.com,ks.stepup.example.com
