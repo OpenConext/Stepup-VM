e47f2931750ff3079e76af85c087e56e3f4ce818
Pieter van der Meulen <pieter.vanderMeulen@surfnet.nl>
2016-10-07 15:21:42 +0200

    Run site.yml ansible-playbook on app server

--- deploy-site-app.sh
+++ deploy-site-app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo ansible-playbook deploy/site.yml -i environment/inventory -l app* -e "galera_bootstrap_node: app.stepup.example.com"
-ansible-playbook deploy/site.yml -i environment/inventory -l app* -e "galera_bootstrap_node: app.stepup.example.com"
+echo ansible-playbook deploy/site.yml -i environment/inventory -l app* -e "galera_bootstrap_node=app.stepup.example.com"
+ansible-playbook deploy/site.yml -i environment/inventory -l app* -e "galera_bootstrap_node=app.stepup.example.com"
