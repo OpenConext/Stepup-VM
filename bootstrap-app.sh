89102e6ec9aa73dcc84725dd819bf7a4bbee98a9
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2023-05-08 17:29:14 +0200

    Make bootstrap command work with both Docker and Vagrant

--- bootstrap-app.sh
+++ bootstrap-app.sh
@@ -1,11 +1,44 @@
 #!/bin/bash
 
-vagrant ssh -c "echo y | sudo /root/01-gateway-db_migrate.sh" app.stepup.example.com
-vagrant ssh -c "echo y | sudo /root/01-middleware-db_migrate.sh" app.stepup.example.com
-vagrant ssh -c "sudo /root/01-keyserver-db_init.sh" app.stepup.example.com
-vagrant ssh -c "sudo /root/01-tiqr-db_init.sh" app.stepup.example.com
-vagrant ssh -c "sudo /root/02-middleware-config.sh" app.stepup.example.com
-vagrant ssh -c "sudo /root/04-middleware-whitelist.sh" app.stepup.example.com
-vagrant ssh -c "sudo /root/05-middleware-institution.sh" app.stepup.example.com
-# Note that 06-middleware-bootstrap-sraa-users.sh is NOT idempotent
-vagrant ssh -c "sudo /root/06-middleware-bootstrap-sraa-users.sh" app.stepup.example.com
+# Get vm_type
+if [ "$v1" != "--vagant" ] && [ "$1" != "--docker" ]; then
+    echo "Usage: $0 --vagrant|--docker"
+    exit 1
+fi
+
+# Remove the first to characters from $1
+vm_type=$(echo $1 | cut -c3- )
+
+sudo=""
+# Only vagrant VM requires sudo
+if [ "$vm_type" == "vagrant" ]; then
+    sudo="sudo"
+fi
+
+# Array of bootstrap command to run
+commands=(
+    "[ -f '/root/01-middleware-db_migrate.sh' ] && echo y | ${sudo} /root/01-middleware-db_migrate.sh"
+    "[ -f '/root/01-webauthn-db_init.sh' ] && echo y | ${sudo} /root/01-webauthn-db_init.sh"
+    "[ -f '/root/01-keyserver-db_init.sh' ] && ${sudo} /root/01-keyserver-db_init.sh"
+    "[ -f '/root/01-tiqr-db_init.sh' ] && ${sudo} /root/01-tiqr-db_init.sh"
+    "[ -f '/root/02-middleware-config.sh' ] && ${sudo} /root/02-middleware-config.sh"
+    "[ -f '/root/04-middleware-whitelist.sh' ] && ${sudo} /root/04-middleware-whitelist.sh"
+    "[ -f '/root/05-middleware-institution.sh' ] && ${sudo} /root/05-middleware-institution.sh"
+    # Note that 06-middleware-bootstrap-sraa-users.sh is NOT idempotent
+    "[ -f '/root/06-middleware-bootstrap-sraa-users.sh' ] && ${sudo} /root/06-middleware-bootstrap-sraa-users.sh"
+)
+
+# Run commands
+if [ "$vm_type" == "vagant" ]; then
+    for command in "${commands[@]}"; do
+        echo "Running: ${command}"
+        sudo="sudo; "vagrant ssh -c "${command}" app.stepup.example.com
+    done
+fi
+
+if [ "$vm_type" == "docker" ]; then
+    for command in "${commands[@]}"; do
+        echo "Running: ${command}"
+        docker exec -it stepupvm /bin/bash -c "${command}"
+    done
+fi
