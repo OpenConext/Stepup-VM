0fd8d0249800b43c4157d8f7c4989276fe4e41f7
Pieter van der Meulen <pieter.vandermeulen@surf.nl>
2023-05-11 13:58:55 +0200

    Start a stopped container and show tips on success

--- docker-up.sh
+++ docker-up.sh
@@ -5,19 +5,54 @@ set -e
 image="stepupvm:latest"
 # Set image to the first parameter of this script, if it is given
 # You can use this to start a different image, e.g. stepupvm:mytag that
-# you created using: 
+# you created using e.g.:
 #   docker commit stepupvm stepupvm:mytag
+#
+# The container is always named "stepupvm". This means that when using this scrip, you can have
+# only one container running at a time. If you want to run multiple containers, you must use
+# docker directly.
 
+function show_tips()
+{
+    echo
+    echo "Tips:"
+    echo "* You create an image from the container using:"
+    echo "    docker stop stepupvm && docker commit stepupvm stepupvm:mytag"
+    echo
+    echo "  You can then start a new container from that image using:"
+    echo "    ./docker-up.sh stepupvm:mytag"
+    echo "  To do that you must stop and remove a running stepupup container first using:"
+    echo "    docker stop stepupvm && docker rm stepupvm"
+    echo
+    echo "* To log in to the container run:"
+    echo "    ./docker-bash.sh"
+    echo
+    echo "Next steps:"
+    echo "* Create the environment, if you haven't done so already. See README.md"
+    echo
+    echo "* Deploy app role from site.yaml:"
+    echo "    ./deploy-site-app.sh --docker"
+    echo
+    echo "* Deploy Stepup components from prebuild tarballs"
+    echo "    ./deploy-release.sh --docker"
+    echo "* Deploy Stepup components from source"
+    echo "    ./deploy-develop.sh --docker"
+    echo
+}
+
+# Set image to use to create the container from
 if [[ "$1" != "" ]]; then
   image="$1"
 fi
 
 
-# Check whether docker container named stepupvm is running
+# Check whether docker container named stepupvm is already running
 if [[ "$(docker ps -q -f name=stepupvm 2> /dev/null)" != "" ]]; then
-  echo "Docker container stepupvm is already running. Nothing to do"
+  echo "Docker container \"stepupvm\" is already running. Nothing to do"
+  show_tips
   exit 0  
 fi
+
 
 # Check whether docker is using cgroup v1
 # If not, exit with error
@@ -31,6 +66,21 @@ if [[ "$(docker info | grep 'Cgroup Version: 1')" == "" ]]; then
   echo "On linux set the kernel parameter 'systemd.unified_cgroup_hierarchy=0' during boot."
 
   exit 1
+fi
+
+# Check whether docker container named already stepupvm exists
+if [[ "$(docker ps -aq -f name=stepupvm 2> /dev/null)" != "" ]]; then
+  # Exists but is not running
+  echo "Docker container \"stepupvm\" exists but is not running. Starting it."
+  docker start stepupvm
+  if [ $? -ne 0 ]; then
+    echo "Failed to start docker container stepupvm"
+    exit 1
+  fi
+  echo "Started Docker container 'stepupvm'"
+  echo
+  show_tips
+  exit 0
 fi
 
 # Check whether docker image stepupvm:latest exists
@@ -73,6 +123,10 @@ docker run -d --name stepupvm \
            -p $host_ip:80:80      \
            -p $host_ip:443:443    \
            "$image"
+if [ $? -ne 0 ]; then
+  echo "Failed to start docker container stepupvm"
+  exit 1
+fi
 
 #     host:container
 # -p 13306:3306    \ MariaDB
@@ -81,18 +135,5 @@ docker run -d --name stepupvm \
 # -p  2080:80      \ NGINX HTTP
 # -p 20443:443     \ NGINX HTTPS
 
-echo "Docker container 'stepupvm' started."
-echo
-echo "You create an image from the running container using:"
-echo "    docker commit stepupvm stepupvm:mytag"
-echo
-echo "You can then start a new container from that image using:"
-echo "    ./docker-run.sh stepupvm:mytag"
-echo "To do that you must stop and remove the running stepupup container first using:"
-echo "    docker stop stepupvm && docker rm stepupvm"
-echo
-echo "To log in to the container run:"
-echo "    ./docker-bash.sh"
-echo 
-echo "Next steps:"
-echo "- Deploy app role from site.yaml: ./deploy-site-app.sh --docker"
+echo "Started Docker container 'stepupvm' from image '${image}'"
+show_tips
